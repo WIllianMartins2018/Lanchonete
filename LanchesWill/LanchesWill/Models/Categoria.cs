cef5f1075d2e495d12949690ea37413de9fae8f9
WIllianMartins2018 <willian.martins01@hotmail.com>
2022-05-21 20:46:27 -0300

    Criação do Modelo de Domínio
    Criação dos Models Categoria e Lanches

---
+++ LanchesWill/LanchesWill/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace LanchesWill.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
