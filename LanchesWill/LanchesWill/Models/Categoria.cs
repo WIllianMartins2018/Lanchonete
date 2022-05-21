159d844fa3a3518873dc43b356b78890fa23b3d8
WIllianMartins2018 <willian.martins01@hotmail.com>
2022-05-21 20:47:48 -0300

    Atualização do Modelo Categoria
    Inclusão da propriedade de navegação List<Lanche>

--- LanchesWill/LanchesWill/Models/Categoria.cs
+++ LanchesWill/LanchesWill/Models/Categoria.cs
@@ -5,5 +5,7 @@
         public int CategoriaId { get; set; }
         public string CategoriaNome { get; set; }
         public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }   
     }
 }
