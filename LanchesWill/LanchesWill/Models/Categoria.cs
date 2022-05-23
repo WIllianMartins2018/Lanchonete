ef28ecc9080d01864c75953f3f8baf58e77a8911
WIllianMartins2018 <willian.martins01@hotmail.com>
2022-05-23 09:49:03 -0300

    Aplicação dos Data Annotation

--- LanchesWill/LanchesWill/Models/Categoria.cs
+++ LanchesWill/LanchesWill/Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace LanchesWill.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesWill.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100,ErrorMessage ="O tamanho máximo é 100")]
+        [Required(ErrorMessage ="Informe o nome da Categoria")]
+        [Display(Name ="Nome")]
         public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200")]
+        [Required(ErrorMessage = "Informe a Descrição")]
+        [Display(Name = "Nome")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }   
