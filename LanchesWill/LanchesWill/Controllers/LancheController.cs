325806741cc9400e7c304234a5552c39415adf26
WIllianMartins2018 <willian.martins01@hotmail.com>
2022-05-29 16:26:40 -0300

    Ajustando view List de Lanches

--- LanchesWill/LanchesWill/Controllers/LancheController.cs
+++ LanchesWill/LanchesWill/Controllers/LancheController.cs
@@ -14,14 +14,14 @@ namespace LanchesWill.Controllers
 
         public IActionResult List()
         {
-            ViewData["Titulo"] = "Todos os Lanches";
-            ViewData["Data"] = DateTime.Now;
+// ViewData["Titulo"] = "Todos os Lanches";
+  //          ViewData["Data"] = DateTime.Now;
 
             var lanches = _lancheRepository.Lanches;
             var totalLanches = lanches.Count();
 
-            ViewBag.Total = "Total de Lanches: ";
-            ViewBag.TotalLanches = totalLanches;
+    //        ViewBag.Total = "Total de Lanches: ";
+      //      ViewBag.TotalLanches = totalLanches;
 
             return View(lanches);
         }
