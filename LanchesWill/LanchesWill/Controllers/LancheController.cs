7c62c5e7c298b86455aac3efab73f83e372e1f3a
WIllianMartins2018 <willian.martins01@hotmail.com>
2022-10-13 21:46:36 -0300

    feature/add-tagHelper-email

--- LanchesWill/LanchesWill/Controllers/LancheController.cs
+++ LanchesWill/LanchesWill/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesWill.Repositories.Interfaces;
+﻿using LanchesWill.Models;
+using LanchesWill.Repositories.Interfaces;
 using LanchesWill.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,15 +14,30 @@ namespace LanchesWill.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-            var lanchesListViewModel = new LancheListViewModel();
+            IEnumerable<Lanche> lanches;
+            string cateogiraAtual = string.Empty;
 
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                cateogiraAtual = "TODOS OS LANCHES";
+            }
+            else
+            {
+                lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals(categoria, StringComparison.OrdinalIgnoreCase))
+                    .OrderBy(l => l.Nome);
+                cateogiraAtual = categoria;
+            }
+
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = cateogiraAtual
+            };
 
             return View(lanchesListViewModel);
-
         }
     }
 }
